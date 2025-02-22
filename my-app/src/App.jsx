9a92717cd12352696be3079b3f515c747aa3cce1
Abhijeet <abhijeetmavoori45@gmail.com>
2025-02-22 12:54:19 +0530

    Initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login1 from './components/Login1'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Login1/>
+    </>
+  )
+}
+
+export default App
